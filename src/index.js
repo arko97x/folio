46c6af3873158aced59426b0fd9ff523971b3cfe
Arko <bhattacharjee.arkoprabho@gmail.com>
2020-09-06 08:38:11 +0530

    fix: testing 3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "pages";
+import App from "./App";
+import Modal from "react-modal";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
-// import App from "./App";
-// import Modal from "react-modal";
+Modal.setAppElement("#root");
 
-// Modal.setAppElement("#root");
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
