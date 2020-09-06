e985ac92278a3b30bc0cfafcd6137d9260339fd1
Arko <bhattacharjee.arkoprabho@gmail.com>
2020-09-06 07:26:38 +0530

    fix: testing a routing fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import ComponentRenderer from "ComponentRenderer.js";
 import Hi from "hi.js";
 
+// import About from "pages/about.js";
+import Home from "pages/home.js";
+// import InFlo from "pages/inflo.js";
+// import ChehakChirps from "pages/chehakchirps.js";
+// import WhatsappLists from "pages/whatsapp-lists.js";
+// import VisualDesigns from "pages/visual-designs.js";
+// import PortfolioRedesign from "pages/portfolio-redesign.js";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -23,6 +31,9 @@ export default function App() {
         </Route>
         <Route path="/folio">
           <Hi />
+        </Route>
+        <Route path="/folio/home">
+          <Home />
         </Route>
       </Switch>
     </Router>
