63e8e940639e0f43e824587764038b84e4feb6eb
Arko <bhattacharjee.arkoprabho@gmail.com>
2020-09-06 07:54:26 +0530

    fix: pls work 😰

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import ComponentRenderer from "ComponentRenderer.js";
 import Hi from "hi.js";
 
-// import About from "pages/about.js";
+import About from "pages/about.js";
 import Home from "pages/home.js";
-// import InFlo from "pages/inflo.js";
-// import ChehakChirps from "pages/chehakchirps.js";
-// import WhatsappLists from "pages/whatsapp-lists.js";
-// import VisualDesigns from "pages/visual-designs.js";
-// import PortfolioRedesign from "pages/portfolio-redesign.js";
+import InFlo from "pages/inflo.js";
+import ChehakChirps from "pages/chehakchirps.js";
+import WhatsappLists from "pages/whatsapp-lists.js";
+import VisualDesigns from "pages/visual-designs.js";
+import PortfolioRedesign from "pages/portfolio-redesign.js";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -26,15 +26,33 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/folio/:name">
-          <ComponentRenderer />
-        </Route>
-        <Route path="/folio">
-          <Hi />
-        </Route>
-        <Route path="/folio/home">
-          <Home />
-        </Route>
+        <Route exact path="/folio/:name" component={ComponentRenderer} />
+        {/* <ComponentRenderer />
+        </Route> */}
+        <Route exact path="/folio" component={Hi} />
+        {/* <Hi />
+        </Route> */}
+        <Route exact path="/folio/home" component={Home} />
+        {/* <Home />
+        </Route> */}
+        <Route exact path="/folio/about" component={About} />
+        {/* <About />
+        </Route> */}
+        <Route exact path="/folio/inflo" component={InFlo} />
+        {/* <InFlo />
+        </Route> */}
+        <Route exact path="/folio/chehakchirps" component={ChehakChirps} />
+        {/* <ChehakChirps />
+        </Route> */}
+        <Route exact path="/folio/portfolio_redesign" component={PortfolioRedesign} />
+        {/* <PortfolioRedesign />
+        </Route> */}
+        <Route exact path="/folio/whatsapp_lists" component={WhatsappLists} />
+        {/* <WhatsappLists />
+        </Route> */}
+        <Route exact path="/folio/visual_designs" component={VisualDesigns} />
+        {/* <VisualDesigns />
+        </Route> */}
       </Switch>
     </Router>
   );
